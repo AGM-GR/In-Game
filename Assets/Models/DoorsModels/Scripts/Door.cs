ab298a2eff5a01ea71666b677149d466944734bf
Alejandro Guerrero Martínez <alejandroguerrero.gr@gmail.com>
2018-04-13 01:05:47 +0200

    Añadidos joins de la puerta y sus límites.

---
+++ Assets/Models/DoorsModels/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public GameObject leaf;
+	public GameObject handle;
+
+	public bool startLocked = true;
+
+	Rigidbody leafRigidbody;
+	HingeJoint handleJoint;
+
+	void Awake () {
+
+		leafRigidbody = leaf.GetComponent<Rigidbody> ();
+		handleJoint = handle.GetComponent<HingeJoint> ();
+	}
+
+	void Start () {
+
+		if (startLocked)
+			LockDoor ();
+	}
+
+	void LockDoor () {
+
+		leafRigidbody.isKinematic = true;
+	}
+
+	void UnlockDoor () {
+
+		leafRigidbody.isKinematic = false;
+	}
+
+	void Update () {
+
+		if (handleJoint.angle <= -40)
+			UnlockDoor ();
+	}
+}
